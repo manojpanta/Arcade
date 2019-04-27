e2474780b5b87721ec2781fd16218a35fc955eb4
manojpanta <manojpanta95@gmail.com>
2019-04-27 11:24:17 -0600

    edit and delete

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -48,5 +48,48 @@ router.post("/", function(req, res, next) {
 });
 
 
+router.put("/:id", function(req, res, next) {
+  Game.update(
+    {
+      title: req.body.title,
+      price: req.body.price,
+      releaseYear: req.body.releaseYear,
+      active: req.body.active
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then(([rowsUpdate, [updatedGame]]) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(202).send(JSON.stringify(updatedGame));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({error})
+    });
+});
+
+router.delete("/:id", function(req, res, next) {
+  Game.destroy(
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then(game => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(202).send(JSON.stringify({success: "Game was deleted!!"}));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({error})
+    });
+});
+
+
 
 module.exports = router; //this should stay at the bottom of the file
